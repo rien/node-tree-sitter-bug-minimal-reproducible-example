d7c64086d7b9b62363f1c43f45827b1dd0577125
Rien Maertens <rien.maertens@posteo.be>
2019-11-19 10:34:04 +0100

    Test if tree-sitter is functional

--- src/tests/test39.ava.js
+++ src/tests/test39.ava.js
@@ -1,7 +1,16 @@
 const test = require("ava");
+const fs = require("fs");
 const Tokenizer = require("../lib/tokenizer");
+
+const expected = "(program (function_declaration name: (identifier) parameters: (formal_parameters) body: (statement_block (return_statement argument: (binary_expression left: (number) right: (number))))))";
 
 test('can be constructed', t => {
 	const tokenizer = new Tokenizer("javascript");
   t.false(typeof tokenizer == undefined || tokenizer == null);
+});
+
+test('can parse', t => {
+	const tokenizer = new Tokenizer("javascript");
+  const program = fs.readFileSync("sample.js");
+  t.is(tokenizer.tokenize(program), expected);
 });
--- src/tests/two.ava.js
+++
@@ -1,7 +0,0 @@
-const test = require("ava");
-const Tokenizer = require("../lib/tokenizer.js");
-
-test('can be constructed', t => {
-	const tokenizer = new Tokenizer("javascript");
-  t.false(typeof tokenizer == undefined || tokenizer == null);
-});
